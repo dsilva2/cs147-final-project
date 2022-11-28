ffaaaf9a00c0d2ea020c36ffb0e2f37bbf6fa974
dsilva2 <dsilva24@stanford.edu>
2022-11-27 22:05:58 -0800

    home screen structure

--- App.js
+++ App.js
@@ -2,37 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Icons from './assets/Icons';
 import themes from './assets/Themes/themes';
+import HomeScreen from './app/components/blank_home_screen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>daha</Text>
-      <View style={styles.homeBar}>
-        <Image
-        source={Icons.home.dark}
-        style={styles.homeBarButton}>
-        </Image>
-
-        <Image
-        source={Icons.search.light}
-        style={styles.homeBarButton}>
-        </Image>
-
-        <Image
-        source={Icons.post.dark}
-        style={styles.postButton}>
-        </Image>
-
-        <Image
-        source={Icons.message.light}
-        style={styles.homeBarButton}>
-        </Image>
-
-        <Image
-        source={Icons.profile.dark}
-        style={styles.homeBarButton}>
-        </Image>
-      </View>
+      <HomeScreen></HomeScreen>
       <StatusBar style="auto" />
     </View>
   );
@@ -45,26 +20,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
-  titleText: {
-    //fontFamily: 'Lato-Black',
-    color: themes.colors.orange,
-    fontSize: 48,
-    textAlign: "center",
-    marginTop: "10%"
-  },
-  homeBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
-    alignItems: 'center',
-    paddingBottom: '5%',
-  },
-  homeBarButton: {
-    width: 48,
-    height: 48,
-  },
-  postButton: {
-    width: 72,
-    height: 72,
-  }
 });
---
+++ app/components/blank_home_screen.js
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Icons from "../../assets/Icons";
+import themes from '../../assets/Themes/themes';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>daha</Text>
+      <View style={styles.homeBar}>
+        <Image
+        source={Icons.home.dark}
+        style={styles.homeBarButton}>
+        </Image>
+
+        <Image
+        source={Icons.search.light}
+        style={styles.homeBarButton}>
+        </Image>
+
+        <Image
+        source={Icons.post.dark}
+        style={styles.postButton}>
+        </Image>
+
+        <Image
+        source={Icons.message.light}
+        style={styles.homeBarButton}>
+        </Image>
+
+        <Image
+        source={Icons.profile.dark}
+        style={styles.homeBarButton}>
+        </Image>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: themes.colors.white,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  titleText: {
+    //fontFamily: 'Lato-Black',
+    color: themes.colors.orange,
+    fontSize: 48,
+    textAlign: "center",
+    marginTop: "10%"
+  },
+  homeBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    alignItems: 'center',
+    paddingBottom: '5%',
+  },
+  homeBarButton: {
+    width: 48,
+    height: 48,
+  },
+  postButton: {
+    width: 72,
+    height: 72,
+  }
+});
